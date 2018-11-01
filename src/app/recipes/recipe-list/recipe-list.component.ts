cf1a4bf27421d50016e9cdfc927e584cffa85949
Robert Franklin <eyesonrobert@gmail.com>
2018-11-01 16:42:15 -0700

    Services Being Implemented
    Services Being Implemented

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: "app-recipe-list",
@@ -7,20 +8,17 @@ import { Recipe } from "../recipe.model";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    //Recipe is a model we created and imported
-    new Recipe(
-      "A Test Recipe",
-      "Simply a Test",
-      "https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg"
-    ),
-    new Recipe(
-      "A Test Recipe",
-      "Simply a Test",
-      "https://media3.s-nbcnews.com/j/MSNBC/Components/Video/201808/tdy_food_klg_chicken_180828_1920x1080.today-inline-vid-featured-desktop.jpg"
-    )
-  ];
-  constructor() {}
+  @Output()
+  recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  ngOnInit() {}
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
